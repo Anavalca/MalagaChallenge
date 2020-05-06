20b4bc9dc089718045d3ecdb4951bf1e7a8c3876
AnaValca <avc92j@gmail.com>
2020-05-06 10:58:17 +0200

    add menu structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import '../stylesheets/App.scss';
 import Landing from './Landing';
-// import errorImg from '../images/MortySad.gif'
 // import { Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
 
   render() {
-    // const { data, value, isFound, ischeckedAlien, ischeckedHuman, theme, ischeckedFemale } = this.state
+
     return (
       <div className="App">
        <header>
          <p className="logo">#ComparteAlegría</p>
-         <p className="video">Ver vídeo<i class="far fa-play-circle"></i></p>
+         <nav className='menu'>
+          <p className="video">Ver vídeo<i class="far fa-play-circle"></i></p>
+          <p className="character">Buscadores de alegría</p>
+         </nav>
        </header>
        <Landing/>
       </div>
