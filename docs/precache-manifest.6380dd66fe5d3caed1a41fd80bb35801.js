c87f6e37b9e71632810a0bd673ee999d7a0d2d92
AnaValca <avc92j@gmail.com>
2020-05-06 11:00:01 +0200

    first docs

---
+++ docs/precache-manifest.6380dd66fe5d3caed1a41fd80bb35801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682a247b64d60c61e472bf2dae58e725",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dfe6100980b08139df21",
+    "url": "./static/css/main.b4dfe64b.chunk.css"
+  },
+  {
+    "revision": "04ff4be8308576b9e319",
+    "url": "./static/js/2.a357256a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a357256a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe6100980b08139df21",
+    "url": "./static/js/main.38363b87.chunk.js"
+  },
+  {
+    "revision": "c70c400507ac0d03dba1",
+    "url": "./static/js/runtime-main.793ca9c6.js"
+  },
+  {
+    "revision": "d555dbe4b049bb195fc341bd14fd85d6",
+    "url": "./static/media/horizonte.d555dbe4.jpg"
+  }
+]);
